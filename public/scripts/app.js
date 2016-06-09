bdf4e1a4483afa98b559aa0715fef2cb09cc1015
Wayne Banks <thestorerun@yahoo.com>
2016-06-08 20:58:05 -0700

    more markers, galore! Why is my map glitching in the center??git add .

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,18 +6,30 @@ var map;
 
 function initMap(data) {
   for (var i = 0; i < data.length; i++) {
-        var myLatLng = {lat: data[0].latitude, lng: data[0].longitude}
+        var myLatLng = {lat: data[i].latitude, lng: data[i].longitude}
         map = new google.maps.Map(document.getElementById('map'), {
           center: myLatLng,
-          zoom: 16
+          zoom: 10
         });
 
-        var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: 'Hello World!'
-        });
-marker.setMap(map);
+        // var marker = new google.maps.Marker({
+        //   position: myLatLng,
+        //   map: map,
+        //   title: 'Hello World!'
+        // });
+        setMarkers(map);
       }
-};
+}
+
+function setMarkers(map) {
+  for (var i = 0; i < data.length; i++) {
+    var venueMark = data[i];
+    var marker = new google.maps.Marker({
+      position: {lat: venueMark.latitude, lng: venueMark.longitude},
+      map: map,
+      title: data.name,
+      venueName: data.venueName
+    });
+  }
+}
 
